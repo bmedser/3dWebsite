42be4e2758be47b06a656fcfd3dd2728ac68cefe
Brendan <bmedser@gmail.com>
2024-03-21 01:21:08 +1100

    v0.1

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
 // Dependencies
 import * as THREE from 'three'
+import gsap from 'gsap'
+import './style.css'
+import { OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import webGL from "three/addons/capabilities/WebGL.js";
+
+/**
+ * Cursor
+ *
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width -0.5
+    cursor.y = -(event.clientY / sizes.height -0.5)
+
+    console.log("X: ", cursor.x, "Y: ", cursor.y)
+})
+
+
 
 // Canvas, target the canvas element to the webgl class
 const canvas = document.querySelector('canvas.webgl')
@@ -64,11 +86,20 @@ const sizes = {
 
 /**
  * Camera
- * @type {PerspectiveCamera}
+ *
  */
-const camera = new THREE.PerspectiveCamera(150, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(150, sizes.width / sizes.height, 0.1, 10000)
+/*const aspectRatio = (sizes.width / sizes.height)
+const camera = new THREE.OrthographicCamera(
+    -4 * aspectRatio,
+    4 * aspectRatio,
+    4,
+    -4,
+    0.1, 100)*/
+
+
 //  We need to move the camera BACKWARDS, so we can see the object render
-camera.position.x = -1 // POV
+camera.position.x = 0 // POV
 camera.position.z = -1 // POV
 scene.add(camera)
 
@@ -84,12 +115,22 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+controls.update()
 
 /**
  * Animate / On Tick Update
  */
-
 const clock = new THREE.Clock()
+
+gsap.to(cube.position, { duration: 5, delay: 2, x:-1 })
+gsap.to(cube.position, { duration: 10, delay: 6, x:2 })
+
 // const clock = new THREE.Clock()
 const tick = () => {
 
@@ -98,9 +139,16 @@ const tick = () => {
 
     //Seconds is the left-most number
     const elapsedTime = clock.getElapsedTime()
+    // console.log("Elapsed time: ", elapsedTime)
 
-    console.log("Elapsed time: ", elapsedTime)
-    console.log()
+    // Make Camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 1.5
+    camera.lookAt(cube.position)
+
+    controls.update()
+
 
     // Update Objects
     cube3.rotation.x = Math.sin(elapsedTime) * 0.35
@@ -115,9 +163,7 @@ const tick = () => {
 
     cube2.rotation.x = elapsedTime
 
-    camera.position.z += Math.cos(elapsedTime) * 0.1
-
-    camera.lookAt(cube.position)
+    // camera.position.z += Math.cos(elapsedTime) * 0.1
 
 
     // Keep updating render every tick
